6d9b75adab3fb533858d29ac3bb9d1bf5ad706b1
moreant <moreant@foxmail.com>
2020-05-29 01:41:49 +0800

    feat: 注册布局

---
+++ app/src/main/java/top/yeek/gdmec_boxuegu/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package top.yeek.gdmec_boxuegu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import top.yeek.gdmec_boxuegu.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
