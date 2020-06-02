429c966fd8471b106c779f53a0e940fb7695e89a
moreant <moreant@foxmail.com>
2020-06-03 00:23:50 +0800

    feat(0428): 读取 SQLite 中的信息和修改性别

--- app/src/main/java/top/yeek/gdmec_boxuegu/activity/UserInfoActivity.java
+++ app/src/main/java/top/yeek/gdmec_boxuegu/activity/UserInfoActivity.java
@@ -1,18 +1,26 @@
 package top.yeek.gdmec_boxuegu.activity;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import top.yeek.gdmec_boxuegu.R;
+import top.yeek.gdmec_boxuegu.bean.UserBean;
+import top.yeek.gdmec_boxuegu.utils.AnalysisUtils;
+import top.yeek.gdmec_boxuegu.utils.DBUtils;
 
-public class UserInfoActivity extends AppCompatActivity{
+public class UserInfoActivity extends AppCompatActivity {
 
     @BindView(R.id.tv_back)
     TextView tvBack;
@@ -20,26 +28,27 @@ public class UserInfoActivity extends AppCompatActivity{
     TextView tvMainTitle;
     @BindView(R.id.title_bar)
     RelativeLayout titleBar;
-    @BindView(R.id.im_avatar)
-    ImageView imAvatar;
-    @BindView(R.id.avatar)
-    RelativeLayout avatar;
-    @BindView(R.id.info_username)
-    TextView infoUsername;
-    @BindView(R.id.username)
-    RelativeLayout username;
-    @BindView(R.id.info_nickname)
-    TextView infoNickname;
-    @BindView(R.id.nickname)
-    RelativeLayout nickname;
-    @BindView(R.id.info_sex)
-    TextView infoSex;
-    @BindView(R.id.sex)
-    RelativeLayout sex;
-    @BindView(R.id.info_signature)
-    TextView infoSignature;
-    @BindView(R.id.signature)
-    RelativeLayout signature;
+    @BindView(R.id.iv_avatar)
+    ImageView ivAvatar;
+    @BindView(R.id.rl_avatar)
+    RelativeLayout rlAvatar;
+    @BindView(R.id.tv_username)
+    TextView tvUsername;
+    @BindView(R.id.rl_username)
+    RelativeLayout rlUsername;
+    @BindView(R.id.tv_nickname)
+    TextView tvNickname;
+    @BindView(R.id.rl_nickname)
+    RelativeLayout rlNickname;
+    @BindView(R.id.tv_sex)
+    TextView tvSex;
+    @BindView(R.id.rl_sex)
+    RelativeLayout rlSex;
+    @BindView(R.id.tv_signature)
+    TextView tvSignature;
+    @BindView(R.id.rl_signature)
+    RelativeLayout rlSignature;
+    private String spUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +57,77 @@ public class UserInfoActivity extends AppCompatActivity{
         ButterKnife.bind(this);
         tvMainTitle.setText("个人资料");
         titleBar.setBackgroundColor(Color.parseColor("#30b4ff"));
+        spUserName = AnalysisUtils.readLoginUserName(this);
+        initData();
+    }
+
+    private void initData() {
+        UserBean bean = null;
+        bean = DBUtils.getInstance(this).getUserInfo(spUserName);
+        if (bean == null) {
+            bean = new UserBean();
+            bean.userName = spUserName;
+            bean.nickName = "moreant";
+            bean.sex = "男";
+            bean.signature = "这个人很懒";
+            DBUtils.getInstance(this).saveUserInfo(bean);
+        }
+        setValue(bean);
+    }
+
+    private void setValue(UserBean bean) {
+        tvNickname.setText(bean.nickName);
+        tvUsername.setText(bean.userName);
+        tvSex.setText(bean.sex);
+        tvSignature.setText(bean.signature);
+    }
+
+
+    @OnClick({R.id.tv_back, R.id.rl_username, R.id.rl_nickname, R.id.rl_sex, R.id.rl_signature})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_back:
+                this.finish();
+                break;
+            case R.id.rl_username:
+                Toast.makeText(this, "username", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rl_nickname:
+                Toast.makeText(this, "nickname", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rl_sex:
+                String sex = tvSex.getText().toString();
+                sexDialog(sex);
+                break;
+            case R.id.rl_signature:
+                Toast.makeText(this, "signature", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private void sexDialog(String sex) {
+        int sexFlag = 0;
+        if ("男".equals(sex)) {
+            sexFlag = 0;
+        } else if ("女".equals(sex)) {
+            sexFlag = 1;
+        }
+        final String items[] = {"男", "女"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("性别");
+        builder.setSingleChoiceItems(items, sexFlag, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(UserInfoActivity.this, items[which], Toast.LENGTH_SHORT).show();
+                setSex(items[which]);
+            }
+        });
+        builder.create().show();
+    }
+
+    private void setSex(String sex) {
+        tvSex.setText(sex);
+        DBUtils.getInstance(this).updateUserInfo("sex", sex, spUserName);
     }
 }
