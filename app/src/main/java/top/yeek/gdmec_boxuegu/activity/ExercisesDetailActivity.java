0d2784dee45adce1dbcdb3d752ebd63457e78226
moreant <moreant@foxmail.com>
2020-06-28 17:15:52 +0800

    feat: 习题详情界面、数据存放

---
+++ app/src/main/java/top/yeek/gdmec_boxuegu/activity/ExercisesDetailActivity.java
@@ -0,0 +1,16 @@
+package top.yeek.gdmec_boxuegu.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import top.yeek.gdmec_boxuegu.R;
+
+public class ExercisesDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercises_detail);
+    }
+}
